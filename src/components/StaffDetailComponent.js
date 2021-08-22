c2f3c0432a9299021db84cfcdcffff72a2e4b641
LongThinh20 <thinhdao202@gmail.com>
2021-08-22 09:37:32 +0700

    fix detail staff layout

--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -11,41 +11,51 @@ import {
   CardBody
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import dateformat from "dateformat";
 
 export default function StaffDetail(props) {
   const { name, doB, startDate, annualLeave, overTime, department, image } =
     props.staff[0];
 
   return (
-    <Container>
-      <Breadcrumb>
-        <BreadcrumbItem>
-          <Link to="/home">Nhân viên</Link>
-        </BreadcrumbItem>
-        <BreadcrumbItem active>{name}</BreadcrumbItem>
-      </Breadcrumb>
-      <div className="row">
-        <div className="col-sm-12 col-md-4 col-lg-3">
-          <img src={image} className="img-fluid" />
+    <section id="staff">
+      <Container>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Nhân viên</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div id="staffDetail">
+          <div className="row">
+            <div className="col-sm-12 col-md-4 col-lg-3">
+              <img src={image} className="img-fluid" />
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-9">
+              <Card>
+                <CardBody>
+                  <dl className="row">
+                    <dt className="col-6">Họ và tên: </dt>
+                    <dd className="col-6">{name}</dd>
+                    <dt className="col-6">Ngày sinh: </dt>
+                    <dd className="col-6"> {dateformat(doB, "dd/MM/yyyy")}</dd>
+                    <dt className="col-6">Ngày vào công ty: </dt>
+                    <dd className="col-6">
+                      {dateformat(startDate, "dd/MM/yyyy")}
+                    </dd>
+                    <dt className="col-6">Phòng ban: </dt>
+                    <dd className="col-6">{department.name}</dd>
+                    <dt className="col-6">Số ngày nghỉ còn lại: </dt>
+                    <dd className="col-6">{annualLeave}</dd>
+                    <dt className="col-6">Số ngày làm thêm: </dt>
+                    <dd className="col-6">{overTime}</dd>
+                  </dl>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
         </div>
-        <div className="col-sm-12 col-md-8 col-lg-9">
-          <Card>
-            <CardHeader className="bg-primary text-white">{name}</CardHeader>
-            <CardBody>
-              <dl className="row">
-                <dt className="col-6">Họ và tên </dt>
-                <dd className="col-6">3 Feb. 2013</dd>
-                <dt className="col-6">Major Stake Holder</dt>
-                <dd className="col-6">HK Fine Foods Inc.</dd>
-                <dt className="col-6">Last Year's Turnover</dt>
-                <dd className="col-6">$1,250,375</dd>
-                <dt className="col-6">Employees</dt>
-                <dd className="col-6">40</dd>
-              </dl>
-            </CardBody>
-          </Card>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </section>
   );
 }
