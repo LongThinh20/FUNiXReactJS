3aba64f7bed0b15bcb171c784c02a50e053a6c42
LongThinh20 <thinhdao202@gmail.com>
2021-08-30 08:10:34 +0700

    install redux

---
+++ src/components/MainComponent.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Footer from "./FooterComponent";
+import Header from "./HeaderComponent";
+import Home from "./HomeComponent";
+import Salary from "./SalaryComponent";
+import Department from "./DepartmentComponent";
+import StaffDetail from "./StaffDetailComponent";
+import { STAFFS, DEPARTMENTS } from "../shared/staffs";
+import { useSelector } from "react-redux";
+
+const Main = () => {
+  const state = useSelector((state) => state.staffs);
+
+  const [resultSearch, setResultSearch] = useState([]);
+  const [staffsListFromState, setStaffsListFromState] = useState([]);
+  const [departmentFormState, setDepartmentFormSate] = useState([]);
+
+  // get data form localStorage
+  useEffect(() => {
+    if (typeof Storage !== "undefined") {
+      localStorage.setItem("STAFFSLIST", JSON.stringify(STAFFS));
+      localStorage.setItem("DEPARTMENTSLIST", JSON.stringify(DEPARTMENTS));
+      setStaffsListFromState(JSON.parse(localStorage.getItem("STAFFSLIST")));
+      setDepartmentFormSate(
+        JSON.parse(localStorage.getItem("DEPARTMENTSLIST"))
+      );
+    } else {
+      alert("Trình duyệt không hổ trợ");
+    }
+  }, []);
+
+  //end get data form localStorage
+
+  const handleAddStaff = (newStaff) => {
+    console.log(newStaff.doB);
+    departmentFormState.forEach((item, index) => {
+      if (item.id.slice(-1) === newStaff.department) {
+        newStaff.department = DEPARTMENTS[`${index}`];
+        item.numberOfStaff = item.numberOfStaff + 1;
+      }
+    });
+
+    const newArr = [...staffsListFromState];
+
+    newArr.push(newStaff);
+
+    setStaffsListFromState(newArr);
+  };
+
+  const handleSearch = (value) => {
+    if (value) {
+      setResultSearch(
+        staffsListFromState.filter((staff) =>
+          staff.name.toUpperCase().includes(value.toUpperCase())
+        )
+      );
+    } else {
+      setResultSearch([]);
+    }
+  };
+
+  const StaffWithId = ({ match }) => {
+    return (
+      <StaffDetail
+        staff={staffsListFromState.filter(
+          (staff) => staff.id === parseInt(match.params.staffId, 10)
+        )}
+      />
+    );
+  };
+
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path="/home"
+          render={() => (
+            <Home
+              staffs={staffsListFromState}
+              handleSearch={handleSearch}
+              resultSearch={resultSearch}
+              handleAddStaff={handleAddStaff}
+              departments={departmentFormState}
+            />
+          )}
+        />
+        <Route path="/home/:staffId" component={StaffWithId} />
+        <Route
+          path="/department"
+          render={() => <Department departments={departmentFormState} />}
+        />
+        <Route
+          path="/salary"
+          render={() => <Salary staffs={staffsListFromState} />}
+        />
+        <Redirect to="/home" />
+      </Switch>
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
---
+++ src/components/StaffDetailComponent.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Container,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import dateformat from "dateformat";
+
+export default function StaffDetail(props) {
+  const { name, doB, startDate, annualLeave, overTime, department, image } =
+    props.staff[0];
+
+  return (
+    <section id="staff">
+      <Container>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Nhân viên</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div id="staffDetail">
+          <h1>Thông tin nhân viên</h1>
+          <div className="row">
+            <div className="col-sm-12 col-md-4 col-lg-3">
+              <img src={image} className="img-fluid" alt="" />
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-9">
+              <Card>
+                <CardBody>
+                  <dl className="row">
+                    <dt className="col-6">Họ và tên: </dt>
+                    <dd className="col-6">{name}</dd>
+                    <dt className="col-6">Ngày sinh: </dt>
+                    <dd className="col-6"> {dateformat(doB, "dd/mm/yyyy")}</dd>
+                    <dt className="col-6">Ngày vào công ty: </dt>
+                    <dd className="col-6">
+                      {dateformat(startDate, "dd/mm/yyyy")}
+                    </dd>
+                    <dt className="col-6">Phòng ban: </dt>
+                    <dd className="col-6">{department.name}</dd>
+                    <dt className="col-6">Số ngày nghỉ còn lại: </dt>
+                    <dd className="col-6">{annualLeave}</dd>
+                    <dt className="col-6">Số ngày làm thêm: </dt>
+                    <dd className="col-6">{overTime}</dd>
+                  </dl>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
