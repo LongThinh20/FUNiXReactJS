352308056317fbede2a6f04afc051ae14854246e
LongThinh20 <thinhdao202@gmail.com>
2021-08-30 21:32:16 +0700

    Redux Actions

---
+++ src/components/MainComponent.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { DISHES } from "../shared/dishes";
+import Menu from "./MenuComponent";
+import Home from "./HomeComponent";
+import DishDetail from "./DishdetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { addComment } from "../Redux/ActionCreators";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.props.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.props.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+          addComment={this.props.addComment}
+        />
+      );
+    };
+
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.props.dishes} />}
+          />
+          <Route path="/menu/:dishId" component={DishWithId} />
+
+          <Route path="/contactus" component={Contact} />
+
+          <Route
+            path="/aboutus"
+            render={() => <About leaders={this.props.leaders} />}
+          />
+
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  };
+};
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment))
+});
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
