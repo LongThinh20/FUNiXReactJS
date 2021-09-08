98a1460c57dd0966b3d777f3d9f70ef5a29d6fc5
LongThinh20 <thinhdao202@gmail.com>
2021-09-08 09:32:51 +0700

    React Redux Forms Revisited

---
+++ src/components/CommetForm.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Row,
+  Col,
+  Label
+} from "reactstrap";
+import { LocalForm, Control, Errors } from "react-redux-form";
+
+export default function CommetForm(props) {
+  const { buttonLabel, className, dishId, addComment } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const maxlength = (len) => (val) => !val || val.length <= len;
+  const minlength = (len) => (val) => val && val.length >= len;
+
+  const handleSubmit = (values) => {
+    addComment(dishId, values.rating, values.author, values.comment);
+  };
+
+  return (
+    <div>
+      <Button onClick={toggle}>{buttonLabel}</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Submit Comment</ModalHeader>
+        <ModalBody>
+          <LocalForm
+            className="form-control"
+            onSubmit={(values) => handleSubmit(values)}
+          >
+            <Row>
+              <Label htmlFor="rating" md={12}>
+                Rating
+              </Label>
+              <Col md={12}>
+                <Control
+                  type="number"
+                  model=".rating"
+                  id="rating"
+                  name="rating"
+                  className="form-control"
+                  defaultValue="1"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Label htmlFor="name" md={12}>
+                Your Name
+              </Label>
+              <Col md={12}>
+                <Control.text
+                  type="text"
+                  model=".author"
+                  id="author"
+                  name="author"
+                  className="form-control"
+                  placeholder="Your name"
+                  validators={{
+                    maxlength: maxlength(15),
+                    minlength: minlength(3)
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  messages={{
+                    maxlength: "Must be 15 characters or less",
+                    minlength: "Must be greater than 2 characters"
+                  }}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Label htmlFor="comment" md={12}>
+                Commment
+              </Label>
+              <Col md={12}>
+                <Control.textarea
+                  model=".comment"
+                  id="comment"
+                  name="comment"
+                  className="form-control"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12} className="mt-2">
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </LocalForm>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+}
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,114 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import CommetForm from "./CommetForm";
+import { Loading } from "./LoadingComponent";
+
+function RenderDish({ dish }) {
+  if (dish) {
+    return (
+      <div>
+        <Card>
+          <CardImg src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  } else return <div></div>;
+}
+
+function RenderComments({ comments, addComment, dishId }) {
+  if (comments) {
+    return (
+      <div>
+        <Card>
+          <CardBody>
+            <h2>Comments</h2>
+            {comments.map((item, index) => (
+              <div key={index}>
+                <CardText>{item.comment}</CardText>
+                <CardText>
+                  -- {item.author},{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit"
+                  }).format(new Date(Date.parse(item.date)))}{" "}
+                </CardText>
+              </div>
+            ))}
+            <CommetForm
+              buttonLabel={"Submit Comment"}
+              dishId={dishId}
+              addComment={addComment}
+            />
+          </CardBody>
+        </Card>
+      </div>
+    );
+  } else return <div></div>;
+}
+
+const Dishdetail = (props) => {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments
+              comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    <div></div>;
+  }
+};
+
+export default Dishdetail;
