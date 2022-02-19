02639112dbafc3bda0f08884c6f8eab148e44aa7
LongThinh20 <thinhdao202@gmail.com>
2022-02-19 08:43:19 +0700

    Adding the Request Driven Login Solution

---
+++ app.js
@@ -0,0 +1,59 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("61e64581272e3cedf11dfc8a")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://studynodejs:nodejs12345@cluster0.r5jho.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "max@text.com",
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
